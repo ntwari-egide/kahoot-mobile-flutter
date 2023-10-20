0478eacef31e66cd87b971984ab9d0ee96d3b404
ntwari-egide <ntwariegide2@gmail.com>
2023-10-19 23:30:06 -0400

    feat: implement question model

---
+++ lib/model/quiz_question.dart
@@ -0,0 +1,9 @@
+// class to store the details for the quiz question 
+class QuizQuestion {
+
+  const QuizQuestion( this.question, this.answers);
+
+  final String question;
+
+  final List<String> answers;
+}
